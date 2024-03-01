28b5eb961fdf5bc5e69b3921f14c881cb26a7c37
starkrogers07 <77605883+starkrogers07@users.noreply.github.com>
2024-03-01 09:17:50 +0530

    install react js and tailwind css and create the first template

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    // Add more file paths if necessary
+  ],
+  // Other Tailwind CSS configurations...
+};
