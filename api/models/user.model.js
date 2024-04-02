65fdd3dbd9e2ced9c5a05bef5d3cdbd35944cefc
starkrogers07 <apurv.senior@gmail.com>
2024-04-02 16:08:38 +0530

    Profile image Upload Functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
+        default: "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg"
     },
     
 }, {timestamps: true});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App(){
   return(
@@ -15,9 +16,10 @@ export default function App(){
     <Route path='/' element = {<Home/>} />
     <Route path='/sign-up' element = {<SignUp/>}/>
     <Route path='/sign-in' element = {<SignIn/>}/>
-    <Route path='/profile' element = {<Profile/>}/>
     <Route path='/about' element = {<About/>}/>
-
+    <Route element={<PrivateRoute/>}>
+    <Route path='/profile' element = {<Profile/>}/>
+    </Route>
   </Routes>
   </BrowserRouter>
   )
