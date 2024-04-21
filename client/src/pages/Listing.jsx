549618da31457f70aee33700d13fe47ef76d6249
starkrogers07 <apurv.senior@gmail.com>
2024-04-21 16:53:12 +0530

    Add Contact Landlord functionality in listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,7 +14,7 @@ import {
     FaParking,
     FaShare,
   } from 'react-icons/fa';
-
+import Contact from './Contact';
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
@@ -22,8 +22,10 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
     const params = useParams();
     const { currentUser } = useSelector((state) => state.user);
+    console.log(currentUser._id, listing?.userRef);
 
     useEffect(() => {
         const fetchListing = async () => {
@@ -50,7 +52,7 @@ export default function Listing() {
         };
         fetchListing();
     }, [params.listingId]);
-    console.log(loading);
+
 
   return (
     <main>{loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
@@ -130,7 +132,7 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            {/* {currentUser && listing.userRef !== currentUser._id && !contact && (
+             {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
                 onClick={() => setContact(true)}
                 className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
@@ -138,7 +140,7 @@ export default function Listing() {
                 Contact landlord
               </button>
             )}
-            {contact && <Contact listing={listing} />} */}
+            {contact && <Contact listing={listing} />} 
           </div>
         </div>
         )}
