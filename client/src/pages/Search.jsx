1eebbf6722038f2867fe06907b56e7c6efb0a922
starkrogers07 <apurv.senior@gmail.com>
2024-05-02 23:40:02 +0530

    Add Show more listing functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,6 +16,7 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
   console.log(listings);
 
 
@@ -45,9 +46,16 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+
+      if(data.length > 8)
+        setShowMore(true);
+      else
+        setShowMore(false);
+
       setListings(data);
       setLoading(false);
     };
@@ -103,6 +111,24 @@ export default function Search() {
     urlParams.set('order', sidebardata.order)
     const searchQuery = urlParams.toString()
     navigate(`/search?${searchQuery}`)
+  };
+
+  const onShowMoreClick = async() => {
+
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();     
+
+    if(data.length < 9){
+      setShowMore(false);
+    }
+
+    setListings([...listings, ...data]);
+
   };
 
   return (
@@ -228,6 +254,13 @@ export default function Search() {
               <Listingitem key={listing._id} listing={listing} />
             ))
           }
+
+          {showMore && (
+            <button onClick={onShowMoreClick} 
+            className="text-green-700 hover:underline p-7 text-center w-full">
+              Show More
+            </button>
+          )}
 
         </div>
       </div>
