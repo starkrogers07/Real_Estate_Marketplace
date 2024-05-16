5a60d3061687dfa39fd834dfa6b60f47b484fbbd
starkrogers07 <apurv.senior@gmail.com>
2024-05-16 12:16:08 +0530

    More fixes :)

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import ListingItem from "../components/ListingItem";
+import ListingItem from "./ListingItem";
 
 export default function Search() {
   const navigate = useNavigate();
